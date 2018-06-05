7c250ffcccaf55ea62eaf9602e472d9fb7602a38
cltson <cltson@mmsofts.com>
2018-06-05 09:25:50 +0700

    angularjs ajax

--- js/angular-master.js
+++ js/angular-master.js
@@ -1,20 +1,28 @@
 
-var myApp = angular.module("myApp", []);
-myApp.controller('MyController', function($scope) {
-    $scope.info = {
-        firstname: "Mahesh",
-        lastname: "Parashar",
-        subject: [
-            { name: "Physics", marks: 65},
-            { name: "Chemistry", marks: 70},
-            { name: "Math", marks: 75},
-            { name: "English", marks: 80},
-            { name: "Hindi", marks: 85},
-        ],
-        fullname: function(){
-            var infoObject;
-            infoObject = $scope.info;
-            return infoObject.firstname + " " + infoObject.lastname; 
-        }
-    }
+// var myApp = angular.module("myApp", []);
+// myApp.controller('MyController', function($scope) {
+//     $scope.info = {
+//         firstname: "Mahesh",
+//         lastname: "Parashar",
+//         subject: [
+//             { name: "Physics", marks: 65},
+//             { name: "Chemistry", marks: 70},
+//             { name: "Math", marks: 75},
+//             { name: "English", marks: 80},
+//             { name: "Hindi", marks: 85},
+//         ],
+//         fullname: function(){
+//             var infoObject;
+//             infoObject = $scope.info;
+//             return infoObject.firstname + " " + infoObject.lastname; 
+//         }
+//     }
+// });
+
+var myapp = angular.module('myapp', []);
+myapp.controller("MyController", function($scope, $http){
+    var url = "data.txt";
+    $http.get(url).then(function(response){
+        $scope.infos = response.data;
+    });
 });
